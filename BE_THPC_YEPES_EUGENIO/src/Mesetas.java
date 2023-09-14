6c90e679fb7c23a54138a23a321161c157b746c2
EugenYepes <eugenionyepes@gmail.com>
2023-09-14 20:51:04 -0300

    parcial v2.1

--- BE_THPC_YEPES_EUGENIO/src/Mesetas.java
+++ BE_THPC_YEPES_EUGENIO/src/Mesetas.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 public class Mesetas {
 	private static Scanner input = new Scanner(System.in);
+	private static final int MAX_NUM = 100;
+	private static final int MIN_NUM = 0;
 	public static void main(String[] args) {
 		int num = 1, prevNumber = -1, amountMesetas = 0, largeOfMeseta = 1, mostLargeMeseta = -1, numMostLargeMeseta = 0;
 		boolean inMeseta = false;
 		boolean lastTimeInMeseta = false;
 
 		System.out.println("\n*******************Calculadora de mesetas******************\n");
-		System.out.println("Por favor ingrese un numeros del 1 al 100, 0 para finalizar");
+		System.out.println("Por favor ingrese un numeros del " + (MIN_NUM + 1) + " al " + MAX_NUM + ", 0 para finalizar");
 
 		while (num != 0) {
 			// enter data
 			do {
 				num = Integer.parseInt(input.nextLine());
-				if (num < 0 || num > 100) {
-					System.out.println("Error, el numero ingresado debe estar comprendido entre [0, 100], reintente");
+				if (num < MIN_NUM || num > MAX_NUM) {
+					System.out.println("Error, el numero ingresado debe estar comprendido entre [" + MIN_NUM + ", " + MAX_NUM + "], reintente");
 				}
-			} while (num < 0 || num > 100);
-			// process data
+			} while (num < MIN_NUM || num > MAX_NUM);
 
+			// process data
 			inMeseta = num == prevNumber;
 			if (inMeseta) {
 				largeOfMeseta++;
@@ -35,6 +37,7 @@ public class Mesetas {
 			}
 			prevNumber = num;
 		}
+
 		// show data
 		System.out.println("La cantidad de mesetas detectadas fue de " + amountMesetas);
 		if (amountMesetas > 0)
